60dcbda9d9e0c2203e20b473b08957c407930c19
Yoann <Yoann@DEATHSTAR>
2016-11-10 14:08:46 +0100

    Bug fix.

--- Sauron/DesignTimeViewModels/DTCameraViewModel.cs
+++ Sauron/DesignTimeViewModels/DTCameraViewModel.cs
@@ -7,12 +7,6 @@ namespace Sauron.DesignTimeViewModels
         public DTCameraViewModel()
             : base(new Controllers.CameraController()
             {
-                Login = "admin",
-                Password = "@Krilik1",
-                IpAddress = "90.120.188.10",
-                RtspPort = 554,
-                Port = 8000,
-                Protocol = "http"
             })
         {
         }
--- Sauron/ViewModels/MainViewModel.cs
+++ Sauron/ViewModels/MainViewModel.cs
@@ -26,16 +26,6 @@ namespace Sauron.ViewModels
 
         public MainViewModel()
         {
-            CameraViewModels.Add(new CameraViewModel(new Controllers.CameraController()
-            {
-                Login = "admin",
-                Password = "@Krilik1",
-                IpAddress = "90.120.188.10",
-                RtspPort = 554,
-                Port = 8000,
-                Protocol = "http"
-            }));
-            SelectedCameraViewModel = CameraViewModels.FirstOrDefault();
         }
     }
 }
